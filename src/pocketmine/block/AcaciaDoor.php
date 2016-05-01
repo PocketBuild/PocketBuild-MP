52da2db4531e1ce02c14ea71cac76a3d87716a1b
Andrew Hutcheson <duncanamcclean@gmail.com>
2016-05-01 13:36:36 +0100

    Added AcaciaDoor

---
+++ src/pocketmine/block/AcaciaDoor.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * src/pocketmine/block/AcaciaDoor.php
+ *
+ * @package default
+ */
+/*
+
+*/
+namespace pocketmine\block;
+use pocketmine\item\Item;
+use pocketmine\item\Tool;
+class AcaciaDoor extends Door
+{
+    protected $id = self::ACACIA_DOOR_BLOCK;
+    /**
+     *
+     * @param unknown $meta (optional)
+     */
+    public function __construct($meta = 0)
+    {
+        $this->meta = $meta;
+    }
+    /**
+     *
+     * @return unknown
+     */
+    public function getName()
+    {
+        return "Acacia Door Block";
+    }
+    /**
+     *
+     * @return unknown
+     */
+    public function canBeActivated()
+    {
+        return true;
+    }
+    /**
+     *
+     * @return unknown
+     */
+    public function getHardness()
+    {
+        return 3;
+    }
+    /**
+     *
+     * @return unknown
+     */
+    public function getToolType()
+    {
+        return Tool::TYPE_AXE;
+    }
+    /**
+     *
+     * @param Item    $item
+     * @return unknown
+     */
+    public function getDrops(Item $item)
+    {
+        return [
+            [Item::ACACIA_DOOR, 0, 1],
+        ];
+    }
+}
